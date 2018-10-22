f17104406e7e6847ef7f2bc9d559a63d86d6f01a
venkata anil <anilvenkata@redhat.com>
2018-10-22 22:10:02 +0530

    Profile python memory object count

--- neutron_trace_profiler/server.py
+++ neutron_trace_profiler/server.py
@@ -1,4 +1,6 @@
+import gc
 import GreenletProfiler
+import json
 import logging
 import os
 import socket
@@ -11,6 +13,8 @@ from oslo_config import cfg
 
 LOG = logging.getLogger(__name__)
 
+obj_count = {}
+
 
 def ensure_dir(path):
     try:
@@ -18,6 +22,7 @@ def ensure_dir(path):
             os.mkdir(path)
     except Exception:
         LOG.info("makdir %s failed", path)
+        pass
 
 
 class ProfilerHandler(object):
@@ -25,24 +30,48 @@ class ProfilerHandler(object):
     def __call__(self, req):
         taskid = req.headers['X-Neutron-Profiler-taskid']
         action = req.headers['X-Neutron-Profiler-Action']
+        iteration = req.headers.get('X-Neutron-Profiler-Iteration')
+        profiler_type = req.headers.get('X-Neutron-Profiler-Type')
+        if not profiler_type:
+            profiler_type = 'calltrace'
+        LOG.info("Trace Profiler pid %s taskid %s action %s iteration %s"
+                 " profiler_type %s",
+                 os.getpid(), taskid, action, iteration, profiler_type)
         if action == 'start':
-            # (anilvenkata): save self.taskid, helpful in throwing error
-            # if stop called without start
-            GreenletProfiler.set_clock_type('cpu')
-            GreenletProfiler.start()
-            LOG.info("Trace Profiler.start profiling %s ", os.getpid())
+            if profiler_type == 'calltrace':
+                GreenletProfiler.set_clock_type('cpu')
+                GreenletProfiler.start()
+            else:
+                if iteration:
+                    obj_count[iteration] = len(gc.get_objects())
+            LOG.info("anil Trace Profiler.start profiling %s ", os.getpid())
+        if action == 'snapshot':
+            if iteration and profiler_type == 'memory':
+                obj_count[iteration] = len(gc.get_objects())
         elif action == 'stop':
-            GreenletProfiler.stop()
-            LOG.info("Trace Profiler.stop profiling %s ", os.getpid())
-            stats = GreenletProfiler.get_func_stats()
+            LOG.info("anil Trace Profiler.stop profiling %s ", os.getpid())
             trace_path = os.path.join(
                 cfg.CONF.trace_profiler.trace_path, taskid)
             ensure_dir(trace_path)
-            trace_file = os.path.join(trace_path,
-                "{}-{}".format(socket.gethostname(), os.getpid()))
-            LOG.info("Trace Profiler.writing to trace file %s ", trace_file)
-            stats.save(trace_file, cfg.CONF.trace_profiler.trace_format)
-            GreenletProfiler.clear_stats()
+            trace_file = os.path.join(
+                trace_path, "{}-{}".format(socket.gethostname(), os.getpid()))
+            if profiler_type == 'calltrace':
+                GreenletProfiler.stop()
+                stats = GreenletProfiler.get_func_stats()
+                LOG.info("Trace Profiler.writing to trace file %s ",
+                         trace_file)
+                stats.save(trace_file, cfg.CONF.trace_profiler.trace_format)
+                GreenletProfiler.clear_stats()
+            else:
+                obj_file = os.path.join(
+                    trace_path, "{}-{}-objcount".format(socket.gethostname(),
+                                                        os.getpid()))
+                if iteration:
+                    obj_count[iteration] = len(gc.get_objects())
+                with open(obj_file, 'w') as fp:
+                    json.dump(obj_count, fp)
+
+                obj_count.clear()
         else:
             LOG.warning("Invalid profiler action %(action)s with "
                         " taskid %(taskid)s",
